acfd40ab748dba82fc2a5f67b1d9de593f2afac7
bipin-a <aasibipin@gmail.com>
2023-05-31 08:58:03 -0400

    update adversarial_examples.py - causing defunct

--- code/adversarial_examples.py
+++ code/adversarial_examples.py
@@ -1,3 +1,4 @@
+import os
 import transformers 
 import textattack
 import torch
@@ -10,7 +11,7 @@ from textattack.attack_recipes import (
 )
 from datasets import load_dataset
 
-
+MODEL_PATH = "models/"
 
 def load_text_attack_dataset():
     hg_dataset = load_dataset(path='glue', name='cola')
@@ -19,46 +20,41 @@ def load_text_attack_dataset():
             )
     return dataset
 
-def load_language_models():
-    checkpoint_names = ["models/default_params",]
-
-    model = transformers.AutoModelForSequenceClassification.from_pretrained(checkpoint_names[0])
-    tokenizer = transformers.AutoTokenizer.from_pretrained("bert-base-cased")
+def load_language_models(model_name):
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(MODEL_PATH + model_name)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
     language_model = HuggingFaceModelWrapper(model,tokenizer)
-    print(language_model)
+    return language_model
 
-    return language_model,"bert-cola-tuned"
+def run_attacks(attack_model, language_model,model_name, dataset):
 
-def run_attacks(language_model,model_name, dataset):
-
-    attacks = [
-            (A2TYoo2021,"A2TYoo2021"),
-            (BERTAttackLi2020,"BERTAttackLi2020")
-            ]
-    for attack_model,attack_name in attacks:         
-        attack = attack_model.build(language_model)
-        
-        attack_args = textattack.AttackArgs(
-            parallel = True,
-            num_examples=-1,
-            csv_coloring_style = 'html',
-            log_to_csv=f"adv_examples/{attack_name}{model_name}_html.csv",
-            # checkpoint_interval=5,
-            # checkpoint_dir="checkpoints",
-            disable_stdout=True
-        )
+    attack_args = textattack.AttackArgs(
+        parallel = True,
+        num_examples=10,
+        csv_coloring_style = 'html',
+        log_to_csv=f"adv_examples/{attack_model.__name__}{model_name}_html.csv",
+        # checkpoint_interval=5,
+        # checkpoint_dir="checkpoints",
+        disable_stdout=True
+    )
         
     #Building attack
+    attack = attack_model.build(language_model)
     attacker = textattack.Attacker(attack, dataset, attack_args)
     attacker.attack_dataset()
     
 def main():
+    model_names = [f for f in os.listdir(MODEL_PATH)]
     data = load_text_attack_dataset()
-    language_models, model_name = load_language_models()
-    print(language_models, model_name)
-    run_attacks(language_models, model_name, data)
+    attacks = [A2TYoo2021, BERTAttackLi2020]
+    for model_name in model_names:
+        language_model = load_language_models(model_name)
+        for attack in attacks:
+            print(attack.__name__)
+            run_attacks(attack, language_model, model_name, data)
 
 
 if __name__ == "__main__":
+    print("running main")
     main()
 
