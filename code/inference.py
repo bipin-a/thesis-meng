46da0172f29af5bfb8bd6e002f21a6dd87aae224
bipin-a <aasibipin@gmail.com>
2023-06-24 18:42:26 -0400

    update inference pipeline

--- code/inference.py
+++ code/inference.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import logging
 import argparse
 import evaluate
 from torch.utils.data import DataLoader
@@ -21,6 +23,7 @@ def tokenize_function(examples, tokenizer_):
     return tokenizer_(examples["sentence"], padding="max_length", truncation=True)
 
 def load_adv_dataset(dataset_name):
+    logging.info(f"Loading {dataset_name}")
     adv_df = pd.read_csv(f"{ADV_DATASET_PATH}{dataset_name}")[['perturbed_text','ground_truth_output']]
     adv_df = adv_df.rename({"perturbed_text":"sentence", "ground_truth_output":"labels"}, axis=1)
     adv_df['sentence'] = adv_df['sentence'].apply(clean_perturbed_dataset)
@@ -28,6 +31,7 @@ def load_adv_dataset(dataset_name):
     return dataset
 
 def tokenize_dataset(dataset, model_name):
+    logging.info(f"Applying tokenizer for model:{model_name} dataset: {dataset}")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenized_dataset = dataset.map(
         tokenize_function, 
@@ -35,21 +39,22 @@ def tokenize_dataset(dataset, model_name):
         fn_kwargs={"tokenizer_": tokenizer}
         )
     tokenized_dataset = tokenized_dataset.remove_columns(["sentence"])
+    tokenized_dataset.set_format("torch")
     return tokenized_dataset
 
 def evaluate_model(model, eval_adv_dataset):
+    logging.info(f"Evaluating Model with model: {model} and dataset: {eval_adv_dataset}")
     eval_dataloader = DataLoader(eval_adv_dataset, batch_size=8)
-    metric = evaluate.load("accuracy")
+    evaluation_metrics = evaluate.combine(["accuracy", "recall", "precision", "f1"])
     model.eval()
     for batch in eval_dataloader:
-        breakpoint()
         batch = {k: v.to(args.device) for k, v in batch.items()}
         with torch.no_grad():
             outputs = model(**batch)
         logits = outputs.logits
         predictions = torch.argmax(logits, dim=-1)
-        metric.add_batch(predictions=predictions, references=batch["labels"])
-    results = metric.compute()
+        evaluation_metrics.add_batch(predictions=predictions, references=batch["labels"])
+    results = evaluation_metrics.compute()
     return results
 
 def main():
@@ -64,14 +69,15 @@ def main():
             print(model._get_name())
             model.to(args.device)
             results = evaluate_model(model, eval_adv_dataset)
-            #hyperparams = {"model": model_name, "dataset_name":dataset_name}
-            #evaluate.save(f"models/results/adversarial_inference/{dataset_name}_{model_name}-{current_time.strftime('%Y_%m_%d-%H_%M_%S')}.json", **results, **hyperparams)
-            break
+            hyperparams = {"model": model_name, "dataset_name":dataset_name}
+            evaluate.save(f"models/results/adversarial_inference/{dataset_name.split('.')[0]}_{model_name}-{current_time.strftime('%Y_%m_%d-%H_%M_%S')}.json", **results, **hyperparams)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('device', choices=['cuda','cpu'])
     args = parser.parse_args()
+    current_time = datetime.now()
+
     if args.device=="cuda":
         try:
             torch.cuda.is_available() == True
